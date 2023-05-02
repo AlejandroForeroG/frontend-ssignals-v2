84b596c8af403dd7ec91a4bf942804a9ae2dc293
Neve <alejandro.forerog@gmail.com>
2023-05-01 19:31:59 -0500

    cambios al store

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,6 +1,11 @@
-import {configureStore} from '@reduxjs/toolkit';
-
+import { configureStore } from "@reduxjs/toolkit";
+import signalsReducer from "../features/signalsName";
+import { apiSlice } from "../services/apiSlice";
 export const store = configureStore({
-
-})
-
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    signals: signalsReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+});

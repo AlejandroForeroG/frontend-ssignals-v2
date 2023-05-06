aadcb099f4d31e1b3a6ba2873128867e08f36f7f
Neve <alejandro.forerog@gmail.com>
2023-05-06 11:25:26 -0500

    fix  2

---
+++ src/pages/Home/Slides/Slides.jsx
@@ -0,0 +1,182 @@
+//import de librerias externas
+import styled from "styled-components";
+import { useState } from "react";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
+import { TbHealthRecognition } from "react-icons/tb";
+
+//import de componentes internos
+import { SlidesUsers } from "./SlidesUser";
+import { SlidesSignals } from "./SlidesSignals";
+import { SlidesFinal } from "./SlidesFinal";
+
+import { ButtonP } from "../../../components/elements/ButtonP";
+import { useGetUsersQuery } from "../../../store/services/userApi";
+
+import { v } from "../../../styles/Variables";
+
+//funcion principal
+export function Slides({ toggleInit }) {
+
+  //funciones de cambio de estado
+  const [slide, setSlide] = useState(0);
+
+  //funcion para compobar que el elemento este en la basee de datos
+  const { data: users, error, isLoading } = useGetUsersQuery();
+
+  if (isLoading)
+    return (
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <h1>Cargando..</h1>
+      </div>
+    );
+  if (error) return <p>Oh no, there was an error</p>;
+
+  //manejo del corrimiento del slide
+  const handleSlide = () => {
+    setSlide(slide + 1);
+  };
+  const handleSlideBack = () => {
+    setSlide(slide - 1);
+  };
+
+
+  //retorno del componente
+  return (
+    <Container>
+      <SwitchTransition>
+        <CSSTransition
+          classNames="fade"
+          key={slide}
+          addEndListener={(node, done) =>
+            node.addEventListener("transitionend", done, false)
+          }
+        >
+          <div className="contenedor-primario">
+            <div className={slide === 0 ? "contenedor" : "off"}>
+              <div className="contenedorimg">
+                <TbHealthRecognition />
+              </div>
+              <div className="contenedor-texto">
+                <h1 className="primero">Bienvenido 👋</h1>
+                <h2>Este es el sistema de recoleccion de señales</h2>
+                <p>Antes de iniciar necesitamos que llenes algunos campos</p>
+                <div className="preInicio"></div>
+              </div>
+              <div>
+                <ButtonP
+                  text="Adelante!"
+                  color="primary"
+                  handle={handleSlide}
+                ></ButtonP>
+              </div>
+            </div>
+
+            <div className={slide === 1 ? "contenedor" : "off"}>
+              <SlidesUsers
+                slide={slide}
+                handleSlide={handleSlide}
+                handleSlideBack={handleSlideBack}
+                users={users}
+              />
+            </div>
+
+            <div className={slide === 2 ? "contenedor" : "off"}>
+              <SlidesSignals
+                handleSlideBack={handleSlideBack}
+                handleSlide={handleSlide}
+              />
+            </div>
+
+            <div className={slide === 3 ? "contenedor" : "off"}>
+              <SlidesFinal
+                handleSlideBack={handleSlideBack}
+                toggleInit={toggleInit}
+              />
+            </div>
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  .contenedor-primario {
+    width: 95vw;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    z-index: 5;
+
+    .off {
+      display: none;
+    }
+    .contenedor {
+      margin-left: 100px;
+      text-align: center;
+      border-radius: 1rem;
+      padding: 1rem;
+      background-color: #fcfcfc;
+      display: flex;
+      flex-flow: column nowrap;
+      align-items: center;
+      justify-content: space-evenly;
+      width: 50rem;
+      min-height: 25rem;
+      position: absolute;
+      top: 50%;
+      left: 45%;
+      transform: translate(-50%, -50%);
+      z-index: 10;
+      box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+      border-radius: 25px;
+      .contenedorimg {
+        font-size: 7rem;
+        color: ${v.secondaryColor};
+
+        margin-bottom: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .contenedo-texto {
+        margin-top: 0;
+      }
+
+      .buttons {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 1rem;
+      }
+    }
+  }
+
+  .fade-enter {
+    opacity: 0;
+    transform: translateY(100%);
+  }
+  .fade-enter-active {
+    opacity: 1;
+    transform: translateY(0%);
+  }
+  .fade-exit {
+    opacity: 1;
+    transform: translateY(0%);
+  }
+  .fade-exit-active {
+    opacity: 0;
+    transform: translateY(-100%);
+  }
+  .fade-enter-active,
+  .fade-exit-active {
+    transition: opacity 500ms, transform 500ms;
+  }
+`;
---
+++ src/pages/Home/Slides/SlidesUser.jsx
@@ -0,0 +1,120 @@
+//componente el cual se encarga de setear el usuario al cual se le van a tomar las medidas
+//imports
+import styled from "styled-components";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { editUser } from "../../../store/slices/user";
+
+import { ButtonP } from "../../../components/elements/ButtonP";
+import { v } from "../../../styles/Variables";
+
+//funcion princiapal
+export function SlidesUsers({ handleSlide, handleSlideBack, users }) {
+  //manejador de error si el id no llega  a estar en la base de datos
+  const [error, setError] = useState(false);
+  const dispatch = useDispatch();
+
+  //funciones
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = e.target.paciente.value.trim();
+    const selectedPatient = users.find(
+      (patient) => patient.id === parseInt(id)
+    );
+
+    if (selectedPatient) {
+      // Paciente encontrado, realizar acción
+      setError(false);
+      dispatch(editUser(selectedPatient));
+      handleSlide();
+    } else {
+      // Paciente no encontrado, mostrar mensaje de error
+      setError(true);
+    }
+  };
+
+  //return del componente
+  return (
+    <Container>
+      <div className="contenedor-texto">
+        <h1 className="primero">Elige el paciente por su ID 🩺 </h1>
+      </div>
+
+      <div className="contenedor-lista">
+        <form id="myForm" onSubmit={handleSubmit}>
+          <input
+            name="paciente"
+            type="search"
+            list="nombres"
+            className="lista"
+            required
+          />
+          <datalist id="nombres">
+            {users.map((user) => (
+              <option value={`${user.id}`} key={user.id} />
+            ))}
+          </datalist>
+        </form>
+        {error && <p>Uy, no se encontro el paciente 😕, ingresa denuevo</p>}
+
+        <div className="buttons">
+          <ButtonP
+            text="Atras"
+            color="secondary"
+            handle={handleSlideBack}
+          ></ButtonP>
+          <ButtonP
+            text="Siguiente"
+            color="primary"
+            type="submit"
+            form="myForm"
+          ></ButtonP>
+        </div>
+      </div>
+    </Container>
+  );
+}
+
+//#region styles
+const Container = styled.div`
+  #listas {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: stretch;
+  }
+
+  .contenedor-texto {
+    padding: 10px;
+  }
+  p {
+    padding: 10px;
+    color: #e13333;
+    font-weight: bold;
+  }
+
+  .lista {
+    background-color: #d3f0fd;
+    border: 1px solid #d3f0fd;
+
+    box-shadow: rgba(50, 50, 93, 0.1) 0 0 0 1px;
+
+    width: 20rem;
+    height: 2rem;
+    border-radius: 45px;
+    padding: 0 1rem;
+  }
+  .lista:focus {
+    border: 1px solid ${v.primaryColor};
+    outline: none;
+    cursor: pointer;
+  }
+
+  .buttons {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+  }
+`;
+//#endregion
